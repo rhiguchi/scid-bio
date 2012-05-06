7e3168cbaf68b77b02d8ee98661ec5d939500f3b
Ryusuke Higuchi <higuchi@sci-d.co.jp>
2012-05-06 15:34:29 +0900

    Features に List 実装

--- src/main/java/jp/scid/bio/Features.java
+++ src/main/java/jp/scid/bio/Features.java
@@ -2,12 +2,15 @@ package jp.scid.bio;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.RandomAccess;
 
-public class Features implements GenBankAttribute {
+public class Features implements GenBankAttribute, List<Feature>, RandomAccess {
     private final List<Feature> features;
 
     public Features(Builder builder) {
@@ -16,6 +19,106 @@ public class Features implements GenBankAttribute {
 
     public List<Feature> getFeatures() {
         return features;
+    }
+
+    public int size() {
+        return features.size();
+    }
+
+    public boolean isEmpty() {
+        return features.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return features.contains(o);
+    }
+
+    public Iterator<Feature> iterator() {
+        return features.iterator();
+    }
+
+    public Object[] toArray() {
+        return features.toArray();
+    }
+
+    public <T> T[] toArray(T[] a) {
+        return features.toArray(a);
+    }
+
+    public boolean add(Feature e) {
+        throw new UnsupportedOperationException("add");
+    }
+
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException("remove");
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return features.containsAll(c);
+    }
+
+    public boolean addAll(Collection<? extends Feature> c) {
+        throw new UnsupportedOperationException("addAll");
+    }
+
+    public boolean addAll(int index, Collection<? extends Feature> c) {
+        throw new UnsupportedOperationException("addAll");
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException("removeAll");
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("retainAll");
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException("clear");
+    }
+
+    public boolean equals(Object o) {
+        return features.equals(o);
+    }
+
+    public int hashCode() {
+        return features.hashCode();
+    }
+
+    public Feature get(int index) {
+        return features.get(index);
+    }
+
+    public Feature set(int index, Feature element) {
+        throw new UnsupportedOperationException("set");
+    }
+
+    public void add(int index, Feature element) {
+        throw new UnsupportedOperationException("add");
+    }
+
+    public Feature remove(int index) {
+        throw new UnsupportedOperationException("remove");
+    }
+
+    public int indexOf(Object o) {
+        return features.indexOf(o);
+    }
+
+    public int lastIndexOf(Object o) {
+        return features.lastIndexOf(o);
+    }
+
+    public ListIterator<Feature> listIterator() {
+        return features.listIterator();
+    }
+
+    public ListIterator<Feature> listIterator(int index) {
+        return features.listIterator(index);
+    }
+
+    public List<Feature> subList(int fromIndex, int toIndex) {
+        return features.subList(fromIndex, toIndex);
     }
 
     public static class Format extends AbstractAttributeFormat {
